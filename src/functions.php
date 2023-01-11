d7739949252603e0273a16c6cb7a4911f85cf1ea
Rees5 <reesalumasa@gmail.com>
2023-01-12 02:09:36 +0300

    add functions

--- src/functions.php
+++ src/functions.php
@@ -64,13 +64,9 @@ function resetPassword($token,$password,$confirmPassword){
 
 function changePassword($token,$oldPassword,$password,$confirmPassword){
   global $url,$ch;
-  $bearerToken = "Authorization: Bearer $token";
   curl_setopt($ch, CURLOPT_URL, $url.'auth/password/change');
 
-  curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BEARER);
-  curl_setopt($ch,CURLOPT_XOAUTH2_BEARER,$bearerToken);
-
-  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+  curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json',"Authorization: Bearer $token"));
   curl_setopt($ch, CURLOPT_POST, 1);
   curl_setopt($ch, CURLOPT_POSTFIELDS,"oldPassword=$oldPassword&password=$password&confirmPassword=$confirmPassword");
   // receive server response ...
@@ -121,7 +117,66 @@ function resendToken($email){
 }
 
 //Case Notes
+function createCaseNote($token,$data){
+  global $url,$ch;
+  $bearerToken = "Authorization: Bearer $token";
+  curl_setopt($ch, CURLOPT_URL, $url.'casenotes');
 
-$token='eccadcc30d9eaebafb2139d4871b088747413b87';
-echo resendToken('reesalumasa@gmail.com');
+  curl_setopt( $ch, CURLOPT_POSTFIELDS, $data );
+  curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json',"Authorization: Bearer $token"
+));
+  // receive server response ...
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $response = curl_exec($ch);
+
+  $err = curl_error($ch);
+  curl_close($ch);
+  if ($err) {
+    return "cURL Error #:" . $err;
+  } else {
+    return $response;
+  }
+}
+
+function fetchCaseNotes($token){
+  global $url,$ch;
+  $bearerToken = "Authorization: Bearer $token";
+  curl_setopt($ch, CURLOPT_URL, $url.'casenotes');
+  curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json',"Authorization: Bearer $token"));
+  // receive server response ...
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $response = curl_exec($ch);
+
+  $err = curl_error($ch);
+  curl_close($ch);
+  if ($err) {
+    return "cURL Error #:" . $err;
+  } else {
+    return $response;
+  }
+}
+
+function fetchCaseNote($token,$caseID){
+  global $url,$ch;
+  $bearerToken = "Authorization: Bearer $token";
+  curl_setopt($ch, CURLOPT_URL, $url.'casenotes/'.$caseID);
+  curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json',"Authorization: Bearer $token"));
+  // receive server response ...
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $response = curl_exec($ch);
+
+  $err = curl_error($ch);
+  curl_close($ch);
+  if ($err) {
+    return "cURL Error #:" . $err;
+  } else {
+    return $response;
+  }
+}
+
+
+
+$token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwMmJiZjdlZjIxOGVhMTRiMzhmZjRiMyIsImZpcnN0TmFtZSI6IkNvbnN1bGFyIiwibGFzdE5hbWUiOiJHZW5lcmFsIiwiZW1haWwiOiJhbm9uYWJiZXlAZ21haWwuY29tIiwicm9sZSI6Im1hbmFnZXIiLCJpYXQiOjE2MTQyMzQ4OTEsImV4cCI6MTYxNTA5ODg5MX0.QTsZef4uI186tc4DgfF9WmHwEkdeNrx74MHA6MBmtEc';
+echo fetchCaseNote($token,'603e297c07fb3a3afdf5b831');
+//echo changePassword($token,'$oldPassword','$password','$confirmPassword')
 ?>
